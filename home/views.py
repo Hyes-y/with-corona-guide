4030613b803740f27cc24f77843e7cdec5564208
farmerrrrr <74737416+farmerrrrr@users.noreply.github.com>
2021-11-11 16:17:41 +0900

    메뉴 바에서 연결되는 템플릿 추가

--- home/views.py
+++ home/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def intro(request):
     return render(request, 'home/intro.html')
+
+def stepbystepRecovery(request):
+    return render(request, 'home/stepbystepRecovery.html')
+
+def overseasTripInfo(request):
+    return render(request, 'home/overseasTripInfo.html')
