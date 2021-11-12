15542e10b0474ab2a0237258c5c34411a06e6132
juu924 <osj8528@gmail.com>
2021-11-12 17:02:57 +0900

    modify gitignore

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j%w7+!xullvc=-ym@*_66+m*tyt8gk&c(wswb$thw^s%b6j0@q'
+SECRET_KEY = os.path.join(BASE_DIR, 'secrets.json')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
